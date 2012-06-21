8cabbad3122094b40a79c3b9f7252f602a414284
Joshua Lee <LeeJoshuaK@gmail.com>
2012-06-20 21:02:56 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HarvardMeals::Application.config.secret_token = '674ab9129754bb6ec9153cbab3fdc1b855dd957e1ad80916044e848c86dc5c814ac106b9db554b53df5167dce242fddd87fedc35c4b4de8ed00a773c69e9a72c'
